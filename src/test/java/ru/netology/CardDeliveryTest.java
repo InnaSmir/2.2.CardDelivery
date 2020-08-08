ed090959aad49951e47dd12a525583fb160a3e39
Inna Smirnova <moipr@yandex.ru>
2020-08-09 01:12:20 +0300

    Added tests

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
-public class CardOrderTest {
+public class CardDeliveryTest {
     private final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     private final int random_num = (int) (Math.random() * 15);
@@ -19,7 +19,7 @@ public class CardOrderTest {
 
     @Test
     void shouldSubmitRequest() {
-        open("http://localhost:9999");
+        open("http://localhost:7777/");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='date'] input").sendKeys(dateFormat.format(bookingDate));
@@ -27,7 +27,74 @@ public class CardOrderTest {
         $("[data-test-id='phone'] input").setValue("+79210000000");
         $("[data-test-id='agreement'] .checkbox__box").click();
         $(".button").click();
-        $(withText("Успешно!")).waitUntil(visible, 12000);
+        $(withText("Успешно!")).waitUntil(visible, 11000);
 
+    }
+
+    @Test
+    void shouldNotSubmitIfCityIsWrong(){
+        open("http://localhost:7777");
+        $("[data-test-id='city'] input").setValue("Кондопога");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").sendKeys(dateFormat.format(bookingDate));
+        $("[data-test-id='name'] input").setValue("Иван Иванов");
+        $("[data-test-id='phone'] input").setValue("+79210010101");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id='city'] .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldNotSubmitIfBookingIsBeforeThreeDays(){
+        open("http://localhost:7777/");
+        $("[data-test-id='city'] input").setValue("Краснодар");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").sendKeys("10.08.2020");
+        $("[data-test-id='name'] input").setValue("Иван Петров");
+        $("[data-test-id='phone'] input").setValue("+79211111111");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".button").click();
+        $(" [data-test-id='date'] .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    void shouldNotSubmitIfEnglishName(){
+        open("http://localhost:7777");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").sendKeys(dateFormat.format(bookingDate));
+        $("[data-test-id='name'] input").setValue("Ivan Ivanov");
+        $("[data-test-id='phone'] input").setValue("+79210000101");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".button").click();
+        $(" [data-test-id='name'] .input__sub").shouldHave(exactText("Имя и Фамилия " +
+                "указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotSubmitIfPhoneIsWrong(){
+        open("http://localhost:7777");
+        $("[data-test-id='city'] input").setValue("Саратов");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").sendKeys(dateFormat.format(bookingDate));
+        $("[data-test-id='name'] input").setValue("Иван Сидоров");
+        $("[data-test-id='phone'] input").setValue("+792100000000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".button").click();
+        $(" [data-test-id='phone'] .input__sub").shouldHave(exactText("Телефон указан неверно. " +
+                "Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldNotSubmitIfCheckboxUnchecked() {
+        open("http://localhost:7777");
+        $("[data-test-id='city'] input").setValue("Новгород");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").sendKeys(dateFormat.format(bookingDate));
+        $("[data-test-id='name'] input").setValue("Петр Иванов");
+        $("[data-test-id='phone'] input").setValue("+79210020202");
+        $(".button").click();
+        $(".input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки " +
+                "и использования моих персональных данных"));
     }
 }
--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -1,4 +1,35 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
 public class DeliveryCardTest {
+    private final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    private final int random_num = (int) (Math.random() * 15);
+    LocalDate date = LocalDate.now();
+    LocalDate bookingDate = date.plusDays(7 + random_num);
+
+
+    @Test
+    void shouldSearchCityByTwoLetters(){
+        open("http://localhost:7777");
+        $("[data-test-id='city'] input").sendKeys("Мо");
+        $$(".menu-item").find(exactText("Москва")).click();
+        $(".input__icon").click();
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").sendKeys(dateFormat.format(bookingDate));
+        $("[data-test-id='name'] input").setValue("Иван Иванов");
+        $("[data-test-id='phone'] input").setValue("+79210000101");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".button").click();
+        $(withText("Успешно!")).waitUntil(visible, 15000);
+    }
 }
